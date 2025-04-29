d3950c6b143f86dcfcea6a07df9ff2b2d6ebbf28
lucae1 <luca.eggerdinger@web.de>
2025-04-29 22:28:36 +0200

    Update Kelly Calculator code

--- frontend/src/app/calculator/kelly-calculator/page.tsx
+++ frontend/src/app/calculator/kelly-calculator/page.tsx
@@ -1,10 +1,5 @@
-import PageLayout from '@/components/layout/PageLayout';
 import KellyCalculatorClientWrapper from './page.client';
 
 export default function KellyCalculatorPage() {
-  return (
-    <PageLayout>
-      <KellyCalculatorClientWrapper />
-    </PageLayout>
-  );
+  return <KellyCalculatorClientWrapper />;
 }
