52d431c44b2bc9a71f2b8a2b60aa0f9201ad6247
lucae1 <luca.eggerdinger@web.de>
2025-05-03 23:49:18 +0200

    Add custom start.js to debug Strapi initialization

---
+++ backend/start.js
@@ -0,0 +1,12 @@
+const strapi = require('@strapi/strapi');
+console.log('Custom Strapi start...');
+console.log('Node.js version:', process.version);
+console.log('Strapi version:', require('@strapi/strapi/package.json').version);
+console.log('Checking database configuration...');
+try {
+  const config = require('./config/database.js')({ env: () => process.env });
+  console.log('Loaded database config:', JSON.stringify(config, null, 2));
+} catch (error) {
+  console.error('Error loading database config:', error);
+}
+strapi({ dir: '.' }).start();
