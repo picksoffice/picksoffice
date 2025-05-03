09de88ccce1ee44ba6dae84f19dcd9f2bb0d8b82
lucae1 <luca.eggerdinger@web.de>
2025-05-03 23:46:06 +0200

    Add version identifier 3.0 to database.js to confirm loading

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,4 @@
+console.log('Using simplified database.js version 3.0');
 module.exports = ({ env }) => {
   console.log('Loading database configuration...');
   console.log('Environment variables:', Object.keys(env()));
