a46b0cc2146bfc5e538967f94e7c596c53193a4f
PicksOffice Admin <admin@picksoffice.com>
2025-07-16 21:02:11 +0200

    Fix database version mismatch

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-console.log('Using simplified database.js version 4.0');
+console.log('Using simplified database.js version 5.1');
 module.exports = ({ env }) => {
   console.log('Loading database configuration...');
   console.log('Environment variables:', Object.keys(env()));
